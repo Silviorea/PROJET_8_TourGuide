530cafbefedd80285b19b333a1f5032a5b48d11a
Silvio REA <silvio.rea07@gmail.com>
2022-07-12 11:59:11 +0200

    test perf to 100 000

--- TourGuide/src/test/java/tourGuide/TestPerformance.java
+++ TourGuide/src/test/java/tourGuide/TestPerformance.java
@@ -58,7 +58,7 @@ public class TestPerformance {
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 15 minutes
-		InternalTestHelper.setInternalUserNumber(10);
+		InternalTestHelper.setInternalUserNumber(100000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 		Tracker tracker = new Tracker(tourGuideService);
 		tracker.stopTracking();
@@ -92,7 +92,7 @@ public class TestPerformance {
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 20 minutes
-		InternalTestHelper.setInternalUserNumber(10);
+		InternalTestHelper.setInternalUserNumber(100000);
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
